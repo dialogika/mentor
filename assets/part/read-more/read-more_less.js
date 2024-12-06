45e7c27623923947ab14530e7d4f197e46610e69
Yoga Pangestu <yogapangestu16@gmail.com>
2024-12-06 22:08:17 +0700

    fix type & add client section

---
+++ assets/part/read-more/read-more_less.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  $(document).ready(function () {
+    $(".readMore").click(function () {
+      var parent = $(this).closest("p");
+      parent.find(".dots, .readMore").css("display", "none");
+      parent.find(".moreText, .readLess").css("display", "inline");
+    });
+
+    $(".readLess").click(function () {
+      var parent = $(this).closest("p");
+      parent.find(".dots, .readMore").css("display", "inline");
+      parent.find(".moreText, .readLess").css("display", "none");
+    });
+  });
+});
