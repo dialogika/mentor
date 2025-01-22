115fb302e80730bd71f1fb96929afa5a9d5e9901
Yoga Pangestu <yogapangestu16@gmail.com>
2025-01-22 22:58:20 +0700

    update slider

--- assets/part/smooth-slider/smooth-slider.js
+++ assets/part/smooth-slider/smooth-slider.js
@@ -1,86 +1,90 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var MySwiperR = new Swiper(".smooth-swiper-container-top", {
-      direction: "horizontal",
-      loop: true,
-      autoplay: {
-        delay: 1,
-        disableOnInteraction: false,
+  var MySwiperT = new Swiper(".smooth-swiper-container-top", {
+    direction: "horizontal",
+    loop: true,
+    autoplay: {
+      delay: 1,
+      disableOnInteraction: false,
+    },
+    speed: 5000,
+    breakpoints: {
+      320: {
+        slidesPerView: "auto",
+        spaceBetween: 10,
       },
-      speed: 5000,
-      breakpoints: {
-        320: {
-          slidesPerView: "auto",
-          spaceBetween: 10,
-        },
-        480: {
-          slidesPerView: "auto",
-          spaceBetween: 20,
-        },
+      480: {
+        slidesPerView: "auto",
+        spaceBetween: 20,
       },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
-    var MySwiperR = new Swiper(".smooth-swiper-container-middle", {
-      direction: "horizontal",
-      loop: true,
-      autoplay: {
-        delay: 1,
-        disableOnInteraction: false,
-        reverseDirection: true,
-      },
-      speed: 5000,
-      breakpoints: {
-        320: {
-          slidesPerView: "auto",
-          spaceBetween: 10,
-        },
-        480: {
-          slidesPerView: "auto",
-          spaceBetween: 20,
-        },
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
-  
-    var MySwiperL = new Swiper(".smooth-swiper-container-bottom", {
-      direction: "horizontal",
-      loop: true,
-      autoplay: {
-        delay: 1,
-        disableOnInteraction: false,
-      },
-      speed: 5000,
-      breakpoints: {
-        320: {
-          slidesPerView: "auto",
-          spaceBetween: 10,
-        },
-        480: {
-          slidesPerView: "auto",
-          spaceBetween: 20,
-        },
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
   });
-  
+  var MySwiperM = new Swiper(".smooth-swiper-container-middle", {
+    direction: "horizontal",
+    loop: true,
+    autoplay: {
+      delay: 1,
+      disableOnInteraction: false,
+      reverseDirection: true,
+    },
+    speed: 5000,
+    breakpoints: {
+      320: {
+        slidesPerView: "auto",
+        spaceBetween: 10,
+      },
+      480: {
+        slidesPerView: "auto",
+        spaceBetween: 20,
+      },
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var MySwiperB = new Swiper(".smooth-swiper-container-bottom", {
+    direction: "horizontal",
+    loop: true,
+    autoplay: {
+      delay: 1,
+      disableOnInteraction: false,
+    },
+    speed: 5000,
+    breakpoints: {
+      320: {
+        slidesPerView: "auto",
+        spaceBetween: 10,
+      },
+      480: {
+        slidesPerView: "auto",
+        spaceBetween: 20,
+      },
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  // window.addEventListener("resize", () => {
+  //   MySwiperT.update();
+  //   MySwiperM.update();
+  //   MySwiperB.update();
+  // });
+});
