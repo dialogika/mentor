bda719aebdb900150f852fed3d8aa0ee4379e550
Yoga Pangestu <yogapangestu16@gmail.com>
2025-05-13 09:18:02 +0700

    update mentor salsabila

---
+++ assets/js/mentor.js
@@ -0,0 +1,31 @@
+// Data untuk mentor dimasukkan kesini
+const listDataMentor = [
+  {
+    nama: "Bang Jacob",
+    image: "assets/img/mentor/bang-jacob.webp",
+    altImage: "Gambar Kak Bang Jacob",
+    mentoring: [`Offline`],
+    lokasi: [`Tangerang Selatan`],
+    url: "https://www.dialogika.co/mentor/bang-jacob.html",
+    kelamin: "Male",
+    masterSkill: "Learning & Development",
+    pengalaman: "10 Tahun",
+    levelKemampuan: "Pro",
+    teaching: ["Teenage", "Adult"],
+    bahasa: ["Indonesia"],
+    deskripsi: "Seorang Digital Learning Special dengan 5 tahun pemgalaman dan lebih dari 10 tahun menjadi MC ",
+    kelas: ["First Private", "Basic Private"],
+    biography: `I’m Jacob, a Learning Specialist with almost 8 years of working experience in Learning & Development area and also 8 years of experience in Technology industry (mostly EduTech).
+
+I have built a professional career spanning 12 years, beginning in 2013. However, my journey in public speaking started much earlier—back in 2008 during my high school years, when I began working as an MC. To this day, I remain active as an MC, accumulating a total of 17 years of experience in this field.
+
+In the Learning & Development sector, I have over eight years of experience as a Trainer and Learning Specialist. Additionally, I am actively involved in coaching various soft skills, including public speaking, personal branding, creative writing, and personal development through career coaching.
+
+To uphold my professional standards, I have obtained several certifications in areas such as Corporate Training, Professional MC, and Public Speaking.
+
+Throughout my career, I have had the opportunity to collaborate with a diverse range of clients—including individuals, groups, and large corporations—across various industries such as F&B, hospitality, cosmetics, healthcare, e-learning providers, SaaS, political parties, and government ministries.
+
+The main reason I am passionate about being an educator and trainer is my love for sharing knowledge and experience with others. This profession also allows me to connect with remarkable individuals and expand my professional network significantly.`,
+    // Add More Data here
+  },
+];
